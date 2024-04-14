99667ca0655a0d327cd6ea2197a9ea52e13ab6ae
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-04-14 18:03:51 -0500

    Add storage module file

---
+++ src/localStorage.js
@@ -0,0 +1,8 @@
+function saveToLocalStorage(key, data) {
+  try {
+    const serializedData = JSON.stringify(data);
+    localStorage.setItem(key, serializedData);
+  } catch (error) {
+    console.error("Error saving to local storage:", error);
+  }
+}
--- src/project.js
+++
@@ -1,7 +0,0 @@
-function Project(name, description, todos) {
-  this.name = name;
-  this.description = description;
-  this.todos = todos;
-}
-
-export default Project;
