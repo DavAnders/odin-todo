497af9653784285409190c5935b70249ac961792
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-04-15 17:02:13 -0500

    Add class methods

--- src/project.js
+++ src/project.js
@@ -7,6 +7,29 @@ class Project {
   addTodo(todo) {
     this.todos.push(todo);
   }
+
+  deleteTodo(todoID) {
+    this.todos = this.todos.filter((todo) => todo.id !== todoID);
+  }
+
+  getTodoByID(todoID) {
+    return this.todos.find((todo) => todo.id === todoID);
+  }
+
+  updateTodo(todoID, data) {
+    const todo = this.getTodoByID(todoID);
+    if (todo) {
+      todo.title = data.title || todo.title;
+      todo.description = data.description || todo.description;
+      todo.dueDate = data.dueDate || todo.dueDate;
+      todo.priority = data.priority || todo.priority;
+    }
+  }
+
+  markCompleted(todoID) {
+    const todo = this.getTodoByID(todoID);
+    todo.isCompleted = true;
+  }
 }
 
 export default Project;
--- src/todoItem.js
+++ src/todoItem.js
@@ -1,9 +1,11 @@
 class TodoItem {
   constructor(title, description, dueDate, priority) {
+    this.id = Date.now().toString;
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.isCompleted = false;
   }
 }
 
